dcd8d5773951fdbfdd7f5ace81d299169aa34aa7
Patryk Gadziński <pgadzinski@cross-net.eu>
2024-07-03 16:40:48 +0200

    Edit User Account

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   resources :trees
   get 'home/index'
   get 'home/pricing'
